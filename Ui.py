914d82de32685e0886e239927a99f5653a512c25
Rupsa1037 <165894906+Rupsa1037@users.noreply.github.com>
2025-05-17 15:50:30 +0530

    Add files via upload

---
+++ Ui.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+import joblib
+
+#loading and vectorizing the model
+model = joblib.load("rf_emotion_model.pkl")
+vectorizer = joblib.load("rf_vectorizer.pkl")
+
+try:
+    le = joblib.load("label_encoder.pkl")
+    use_label_encoder = True
+except:
+    use_label_encoder = False
+
+#function for precicting emotion
+def predict_emotion(text):
+    vector = vectorizer.transform([text])
+    prediction = model.predict(vector)
+    if use_label_encoder:
+        return le.inverse_transform(prediction)[0]
+    else:
+        return prediction[0]
+
+#layout for app
+st.title(": Emotion Detection from Text :")
+st.subheader(": Enter a sentence to detect the emotion :")
+
+user_input = st.text_area("Your Text", height=150)
+
+if st.button("Detect Emotion"):
+    if user_input.strip() == "":
+        st.warning("Please enter some text!")
+    else:
+        result = predict_emotion(user_input)
+        st.success(f"--> Predicted Emotion: **{result}**")
